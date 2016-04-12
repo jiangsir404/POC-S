f20ebc962ecbcc71ef32965196b43e3e12dced1d
Xyntax <xyntax@163.com>
2016-04-11 21:31:15 -0400

    add ./lib/cmdline.py

--- POC-T.py
+++ POC-T.py
@@ -10,25 +10,25 @@ import optparse
 import glob
 import imp
 from lib.consle_width import getTerminalSize
+from lib.cmdline import parse_args
 
 
 class POC_T:
-    def __init__(self, threads_num, module_name, filepath, output):
+    def __init__(self, threads_num, module_name, filepath, output, f_flag, s_flag):
 
         # 动态加载外部模块
         fp, pathname, description = imp.find_module(module_name, ["module"])
         self.module_obj = imp.load_module("_", fp, pathname, description)
-
         self.filepath = filepath
-
         self.cancel_output = False
+        self.cancel_print = False
+        self.f_flag = f_flag
+        self.s_flag = s_flag
         self.output = output if output else \
             './output/' \
             + time.strftime('%Y%m%d-%H:%M:%S', time.localtime(time.time())) \
             + module_name \
             + '.txt'
-        if self.output in ['N', 'n']:
-            self.cancel_output = True
 
         self.thread_count = self.threads_num = threads_num
         self.scan_count = self.found_count = 0
@@ -66,6 +66,7 @@ class POC_T:
         while self.queue.qsize() > 0:
             payload = self.queue.get(timeout=1.0)
             poced = False
+
             try:
                 if self.module_obj.poc(payload):
                     poced = True
@@ -73,17 +74,22 @@ class POC_T:
                     pass
             except:
                 pass
+
             if poced:  # 不能把open语句放在try块里，因为当打开文件出现异常时，文件对象file_object无法执行close()方法
                 self.lock.acquire()
                 self.found_count += 1
-                if not self.cancel_output:
+                if self.f_flag:
                     f = open(self.output, 'a')
                     f.write(payload + '\n')
                     f.close()
                 self.lock.release()
             self._update_scan_count()
+
+            if self.s_flag:
+                self._print_progress()
+
+        if self.s_flag:
             self._print_progress()
-        self._print_progress()
         self.lock.acquire()
         self.thread_count -= 1
         self.lock.release()
@@ -99,38 +105,12 @@ class POC_T:
 
 
 if __name__ == '__main__':
-
-    print "\n[------POC-T  version 1.0------]  \n From i@cdxy.me http://www.cdxy.me\n"
-    parser = optparse.OptionParser('Usage: %prog [options] -m [module] -f [filepath]\n'
-                                   'Example: python POC-T.py -t 20 -m ssh-brute -f ./iplist.txt')
-
-    parser.add_option('-m', dest='module_name', default=None,
-                      type='string', help='select module/POC name in ./module/')
-    parser.add_option('-f', dest='filepath', default=None,
-                      type='string', help='Input file path&name.')
-    parser.add_option('-t', dest='threads_num',
-                      default=10, type='int',
-                      help='Number of threads. default = 10')
-    parser.add_option('-o', dest='output',
-                      type='string', help='Output file path&name. default in ./output/')
-    parser.add_option('--show', dest='show_module_name', default=False,
-                      action='store_true', help='Show available module/POC names')
-
-    (options, args) = parser.parse_args()
-    if not options.module_name or not options.filepath:
-        parser.print_help()
-        sys.exit(0)
-
-    if options.show_module_name:
-        module_name_list = glob.glob(r'./module/*.py')
-        print 'Num: ' + str(len(module_name_list))
-        for each in module_name_list:
-            print each.split('/')[-1].strip('.py')
-        parser.print_help()
-        sys.exit(0)
-
-    d = POC_T(threads_num=options.threads_num,
-              module_name=options.module_name,
-              filepath=options.filepath,
-              output=options.output)
+    args = parse_args()
+    d = POC_T(threads_num=args.t,
+              module_name=args.m,
+              filepath=args.f,
+              output=args.o,
+              f_flag=args.nF,
+              s_flag=args.nS
+              )
     d.run()
---
+++ lib/cmdline.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+#
+#  Parse command line arguments
+#
+
+import argparse
+import sys
+import os
+import glob
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(prog='POC-T',
+                                     formatter_class=argparse.RawTextHelpFormatter,
+                                     description='From i@cdxy.me http://www.cdxy.me',
+                                     usage='POC-T.py [options]')
+
+    parser.add_argument('-m', metavar='module_name', type=str, default='',
+                        help='Select Module/POC name in ./module/')
+    parser.add_argument('-f', metavar='filepath', type=str, default='',
+                        help='Load targets from TargetFile')
+    parser.add_argument('-i', metavar='int_string', type=str, default='',
+                        help='-i [start_int]-[end_int]: test payloads from int(start) to int(end) with step 1.')
+    parser.add_argument('-t', metavar='threads_num', type=int, default=10,
+                        help='Num of scan threads for each scan process, 20 by default')
+    parser.add_argument('-o', metavar='output', type=str, default='',
+                        help='Output file path&name. default in ./output/')
+    parser.add_argument('--show', default=False, action='store_true',
+                        help='Show available module/POC names')
+    parser.add_argument('--nF', default=True, action='store_false',
+                        help='Disable file output.')
+    parser.add_argument('--nS', default=True, action='store_false',
+                        help='Disable screen output.')
+    parser.add_argument('-v', action='version', version='%(prog)s 1.2    By cdxy (http://www.cdxy.me)')
+
+    if len(sys.argv) == 1:
+        sys.argv.append('-h')
+    args = parser.parse_args()
+
+    check_args(args)
+    return args
+
+
+def check_args(args):
+    if args.show:
+        module_name_list = glob.glob(r'./module/*.py')
+        print '\nModule Num: ' + str(len(module_name_list) - 1)
+        for each in module_name_list:
+            _str = each.split('/')[-1].strip('.py')
+            if _str not in ['__init__']:
+                print _str
+        sys.exit(0)
+
+    if not args.m:
+        msg = 'Use -m to select a module name.'
+        raise Exception(msg)
+    if args.m and not os.path.isfile("./module/" + args.m + ".py"):
+        msg = 'module not exist.\nUse --show to view all available module names.'
+        raise Exception(msg)
+
+    if (not args.f and not args.i) or (args.f and args.i):
+        msg = 'Use -f to set TargetFile or -i to set Payload.'
+        raise Exception(msg)
+    if args.f and not os.path.isfile(args.f):
+        raise Exception('TargetFile not found: %s' % args.f)
+    if args.i:
+        help_str = "invalid input in [-i]\n Example: python POC-T -m test -i 1-100"
+        try:
+            _int = args.i.strip().split('-')
+            if int(_int[0]) < int(_int[1]):
+                print "\n[*] Loading Payloads from " + _int[0] + " to " + _int[1] + " ..\n"
+                # TODO
+            else:
+                print help_str
+                sys.exit(0)
+        except Exception, e:
+            print e
+            print help_str
+            sys.exit(0)
