989bae2d9fced2bc0ed5fdfe61a5888f75c68fa9
boy-hack <34109680@qq.com>
2018-02-23 19:48:05 +0800

    add iis7.5parse

--- fuzz/iisparse.py
+++ fuzz/iisparse.py
@@ -9,13 +9,13 @@ def poc(url):
         url = 'http://' + url
     if not url.endswith('/'):
         url = url + "/"
-    url = url + "/crossdomain.xml"
+    url = url + "robots.txt/.php"
     try:
         header = dict()
         header["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
         r = requests.get(url, headers=header, timeout=5)
-        if 'allow-access-from domain="*"' in r.text:
-            return '[crossdomain.xml domain value is *]'+url
+        if "User-agent" in r.text:
+            return '[iis7.5 parse]'+url
         else:
             return False
     except Exception:
