902d3011361db129b91f0739c4f1332e2862cf10
boy-hack <34109680@qq.com>
2018-06-30 12:37:23 +0800

    patch

--- fuzz/phpinfo.py
+++ fuzz/phpinfo.py
@@ -35,7 +35,7 @@ xxx.php
     result = []
     for path in paths:
         try:
-            payload = url + path
+            payload = url + path.strip()
             header = dict()
             header["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
             r = requests.get(payload, headers=header, timeout=5)
