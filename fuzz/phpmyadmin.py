b9fac1b3e8bcf444edd7f88048fc41c4c90d70a1
boy-hack <34109680@qq.com>
2018-06-30 13:13:38 +0800

    patch for phpmyadmin

--- fuzz/phpmyadmin.py
+++ fuzz/phpmyadmin.py
@@ -10,11 +10,11 @@ def poc(url):
         url = 'http://' + url
     if not url.endswith('/'):
         url = url + "/"
-    url = url + "phpmyadmin/themes/pmahomme/jquery/jquery-ui-1.8.16.custom.css"
+    payload = url + "phpmyadmin/themes/pmahomme/jquery/jquery-ui-1.8.16.custom.css"
     try:
         header = dict()
         header["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
-        r = requests.get(url, headers=header, timeout=5)
+        r = requests.get(payload, headers=header, timeout=5)
         if r.status_code == 200:
             md5_value = md5.new(r.content).hexdigest()
             if md5_value == "2059c4c1ec104e7554df5da1edb07a77":
