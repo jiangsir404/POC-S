8004f6f9c9389d65480a05b6950202ff4c7d50ec
jiangsir404 <2461805286@qq.com>
2020-02-04 20:37:24 +0800

    [OPT] 修改ReadME.md

--- build/lib/pocs/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python		
-#coding:utf-8
-
-import sys
-import os
-
-sys.path.append(os.path.dirname(__file__))
-
-from pocs import main
--- build/lib/pocs/test/myself_script.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python		
-#coding:utf-8
-
-"""
-测试自定义脚本的加载
-"""
-
-def poc(str):
-	return True
