55ead1d416d25275833f5d133cf5a1db0cc7f330
jiangsir404 <2461805286@qq.com>
2020-02-27 20:36:08 +0800

    [ADD] 添加weblogic-wls-xmldecoer.py

---
+++ pocs/script/weblogic/weblogic-wls-xmldecoder.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+"""
+Author: rivir
+Date: 2020/2/27
+
+@描述
+@功能:
+@Usage
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
